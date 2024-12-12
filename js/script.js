1eff1485274492eadf3d0d30328ee1aeefc0b4b4
Dan9 <burdovicen@gmail.com>
2024-12-12 17:12:27 +0400

    add

--- js/script.js
+++
@@ -1,8 +0,0 @@
-(() => {
-const button = document.querySelector('.form-label-sendbtn');
-
-// При наведении на кнопку начинаем анимацию
-button.addEventListener('mouseenter', () => {
-  button.classList.add('animate');  // Добавляем класс для анимации
-});
-})();
